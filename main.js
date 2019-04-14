130a66a43672790224ba0d2173621b45016ac7e9
Michael Zheng <mz6141@gmail.com>
2019-04-13 23:47:17 -0700

    intial changes

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+	$(window).resize(function () {
+		var windowHeight = $(window).height();
+		var margin_top = windowHeight * -0.13;
+
+		$('.home-container-well').css({
+			'height': windowHeight
+			});
+		$('#title').css({
+			'margin-top': margin_top
+			});
+	}).resize();
+
+	function applyFade() {
+		$('.title-inner').addClass('fade-in');
+		$('.panel-footer').addClass('bottom-up');
+	}
+	applyFade();
+
+	$('.social-link').on('click', function() {
+		$this = $(this);
+		$this.addClass('clicked');
+	});
+});
