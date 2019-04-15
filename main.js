2cf15641aba908d977aab7a9084df4f23702a5ca
Michael Zheng <mz6141@gmail.com>
2019-04-14 20:01:12 -0700

    need to fix mobile website

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 /*
 
-$(document).ready(function () {
 
-	$(window).resize(function () {
-		var windowHeight = $(window).height();
-		var margin_top = windowHeight * -0.13;
-
-		$('.home-container-well').css({
-			'height': windowHeight
-			});
-		$('#title').css({
-			'margin-top': margin_top
-			});
-	}).resize();
-
-	function applyFade() {
-		$('.title-inner').addClass('fade-in');
-		$('.panel-footer').addClass('bottom-up');
-	}
-	applyFade();
-
-	$('.social-link').on('click', function() {
-		$this = $(this);
-		$this.addClass('clicked');
+$(document).ready(function() {
+    $( ".email-button" ).click(function() {
+		if($( ".email-button" ).text() === "Email"){
+			$( ".email-button" ).text("michaeldzheng@gmail.com")
+		}
+		else{
+			$( ".email-button" ).text("Email")
+		}
 	});
+
+	$( ".github-button" ).click(function() {
+		if($( ".github-button" ).text() === "Github"){
+			$( ".github-button" ).text("@chickennoodlesoup")
+		}
+		else{
+			$( ".github-button" ).text("Github")
+		}
+	});		
+
+
+
+
+
+
+
 });
+
+
+
 */
